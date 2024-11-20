c85be0934b8eb5dab28e888621810897dc3995aa
newbackjava <hera@naver.com>
2024-11-20 16:02:47 +0900

    20241120

--- src/main/java/org/example/mongo/NaverInsertMain.java
+++ src/main/java/org/example/mongo/NaverInsertMain.java
@@ -1,8 +1,15 @@
 package org.example.mongo;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class NaverInsertMain {
 
     public static void main(String[] args) {
+
+        Logger mongoLogger = Logger.getLogger( "org.mongodb.driver" );
+        mongoLogger.setLevel(Level.WARNING);
+
         MongoDBBlogInsert.insertBlogMember();
         MongoDBCafeInsert.insertCafeMember();
         MongoDBLineInsert.insertLineMember();
